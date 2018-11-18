d08340cc869a235e066fd2a2cc1df1127c483a8d
lizfaria <efaria93@gmail.com>
2018-11-18 18:40:49 -0500

    ingreds

--- refactorscript.js
+++ refactorscript.js
@@ -1,12 +1,44 @@
 const app = {};
 
-app.recipeKeys = function () {
+app.appendElemToUl = function(parent, child) {
+    return parent.appendChild(child);
+}
+
+app.appendHtmlToElem = function(elem, html, recipeItem, className) {
+    const a = document.createElement(elem);
+    const b = document.createTextNode(html);
+    a.appendChild(b);
+    a.classList.add(className);
+    app.appendElemToUl(recipeItem, a);
+    return a;
+}
+
+app.appendHtmlToElemOnly = function(elem, html, className) {
+    const a = document.createElement(elem);
+    const b = document.createTextNode(html);
+    a.appendChild(b);
+    a.classList.add(className);
+    return a;
+}
+
+app.setAttr = function(elem, attr_prop, attr_val) {
+    return elem.setAttribute(attr_prop, attr_val);
+}
+
+app.updateSearchTerm = function () {
     const searchTerm = document.querySelector('input[type = search]').value; 
+    app.filterAllergyInputs(searchTerm);
+}
+
+app.filterAllergyInputs = function(searchTerm) {
     const allAllergies = document.querySelectorAll('.allergies input[type = checkbox]');
     const selectedAllergy = [...allAllergies].filter(input => input.checked == true);
+    app.filterDietaryInputs(searchTerm, selectedAllergy)
+}
+
+app.filterDietaryInputs = function(searchTerm, selectedAllergy) {
     const allDiets = document.querySelectorAll('.dietary input[type = checkbox]');
     const selectedDiet = [...allDiets].filter(input => input.checked == true);
-
     app.getRecipes(searchTerm, selectedAllergy, selectedDiet);
 }
 
@@ -32,30 +64,36 @@ app.getRecipes = function (searchTerm, selectedAllergy, selectedDiet) {
         });
 };
 
-app.appendElemToUl = function(parent, child) {
-    return parent.appendChild(child);
+app.updateSearchTitle = function(searchTerm, recipeArray) {
+    const recipes = document.getElementById('recipes');
+    const searchTitle = document.getElementById('searchTitle');
+    if (searchTitle) {
+        const results = document.getElementById('results');
+        results.removeChild(searchTitle);
+    } 
+    if (recipeArray.length > 0 ) {
+        recipes.insertAdjacentHTML('beforebegin', `<h2 class="search-title" id="searchTitle">${`Search results for ${searchTerm}`}</h2>`)
+    } else {
+        recipes.insertAdjacentHTML('beforebegin', `<h2 class="search-title" id="searchTitle">${`No results for ${searchTerm}`}</h2>`)
+    } 
 }
 
-app.appendHtmlToElem = function(elem, html, recipeItem, className) {
-    const a = document.createElement(elem);
-    const b = document.createTextNode(html);
-    a.appendChild(b);
-    a.classList.add(className);
-    app.appendElemToUl(recipeItem, a);
-    return a;
-}
-
-app.appendHtmlToElemOnly = function(elem, html, className) {
-    const a = document.createElement(elem);
-    const b = document.createTextNode(html);
-    a.appendChild(b);
-    a.classList.add(className);
-    return a;
-}
-
-app.setAttr = function(elem, attr_prop, attr_val) {
-    return elem.setAttribute(attr_prop, attr_val);
-}
+app.displayRecipes = function (recipeArray) {
+    const recipes = document.getElementById('recipes');
+    recipeArray.forEach(item => {
+        const recipeItem = app.appendHtmlToElemOnly('li', "", 'item');
+        
+        app.showRecipeTitle(item, recipeItem);
+        app.showRecipeSource(item, recipeItem);
+        app.showRecipeImage(item, recipeItem);
+        app.createIngredientsUl(item, recipeItem);
+        app.createIngredientsButton(recipeItem);
+        app.appendElemToUl(recipes, recipeItem);
+        
+        recipes.style.display = 'flex';
+    });
+    app.smoothScroll();
+};
 
 app.showRecipeTitle = function (item, recipeItem) {
     const elem = 'h4';
@@ -104,29 +142,12 @@ app.createIngredientsButton = function (recipeItem) {
     app.toggleIngredients(recipeItem, ingredientsButton);
 }
 
-app.displayRecipes = function (recipeArray) {
-    const recipes = document.getElementById('recipes');
-    recipeArray.forEach(item => {
-        const recipeItem = app.appendHtmlToElemOnly('li', "", 'item');
-
-        app.showRecipeTitle(item, recipeItem);
-        app.showRecipeSource(item, recipeItem);
-        app.showRecipeImage(item, recipeItem);
-        app.createIngredientsUl(item, recipeItem);
-        app.createIngredientsButton(recipeItem);
-        app.appendElemToUl(recipes, recipeItem);
-
-        recipes.style.display = 'flex';
-    });
-    app.smoothScroll();
-};
-
 app.toggleIngredients = function (ingredientsButton, recipeItem) {
-        ingredientsButton.addEventListener('click', () => {
-            app.showOrHideIngredients(ingredientsButton, recipeItem);
-        });
+    ingredientsButton.addEventListener('click', () => {
+        app.showOrHideIngredients(ingredientsButton, recipeItem);
+    });
 };
-
+    
 app.showOrHideIngredients = function (recipeItem) {
     const ingredients = recipeItem.children[3];
     if (ingredients.classList.contains('show')) {
@@ -137,31 +158,15 @@ app.showOrHideIngredients = function (recipeItem) {
 };
 
 app.showIngredients = function (ingredients) {
-    // ingredients.classList.remove("hide");
     ingredients.classList.add("show");
 }
 app.hideIngredients = function (ingredients) {
     ingredients.classList.remove("show");
-    // ingredients.classList.add("hide");  
 }
 
 app.emptyPreviousSearchResults = function () {
     const recipes = document.getElementById('recipes');
     recipes.innerHTML = '';
-}
-
-app.updateSearchTitle = function(searchTerm, recipeArray) {
-    const recipes = document.getElementById('recipes');
-    const searchTitle = document.getElementById('searchTitle');
-    if (searchTitle) {
-        const results = document.getElementById('results');
-        results.removeChild(searchTitle);
-    } 
-    if (recipeArray.length > 0 ) {
-        recipes.insertAdjacentHTML('beforebegin', `<h2 class="search-title" id="searchTitle">${`Search results for ${searchTerm}`}</h2>`)
-    } else {
-        recipes.insertAdjacentHTML('beforebegin', `<h2 class="search-title" id="searchTitle">${`No results for ${searchTerm}`}</h2>`)
-    } 
 }
 
 app.updateSearchForm = function () {
@@ -204,7 +209,7 @@ app.events = function () {
     document.getElementById('submit').onclick = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        app.recipeKeys();
+        app.updateSearchTerm();
         app.emptyPreviousSearchResults();
         app.updateSearchForm();
     };
@@ -221,22 +226,20 @@ app.init = function () {
 
 
 // toDo:
-
-// make more PURE -  each function should only do one thing and see where functions can be reused! 
-
-// make a title that says no results for searchterm is it is invalid
-
+// make more pure
 // add directions
+// animation on buttons (direction numbers stretch on select)
 
-// animation on buttons (directions stretch select)
-
-// ES6: 
+// Blog - refactoring from js to ES6
+// ----------------------------------
 // Map
 // filter 
 // axios 
-// for each 
+// foreach 
 // back ticks 
 // reusuable functions 
+// return - often forgot to include this in functions but V IMPORTANT to receive the changes on the element 
+// on click events
 
 // addclasslist
 // document.queryselector - unlike jquerry selector only targets 1 element, so to target all, have to use for each loop
